c443cd66cf23c3729dd9fc5c76edda4624314d31
ZP <991475623@qq.com>
2021-03-28 13:36:13 +0800

    [update]

---
+++ zp-mall-tiny-02/src/test/java/com/zp/mall/tiny/ZpMallTiny01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zp.mall.tiny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZpMallTiny01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
